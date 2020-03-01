827b9628478b773b0ffeb8b0b4cf1172afea1216
Archana Karki <39858235+archanakarki@users.noreply.github.com>
2020-03-01 14:21:03 +0200

    Updates

---
+++ precache-manifest.93557181423310e192f9602ca9d0c77e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45297c101ad3e1b91a827b4ffdeab24f",
+    "url": "/archanakarki/request_giosg_api.git/index.html"
+  },
+  {
+    "revision": "06ff92d6ad489ec40757",
+    "url": "/archanakarki/request_giosg_api.git/static/css/main.6df0fef3.chunk.css"
+  },
+  {
+    "revision": "8d855784df55ec6402d6",
+    "url": "/archanakarki/request_giosg_api.git/static/js/2.9df2154e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/archanakarki/request_giosg_api.git/static/js/2.9df2154e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06ff92d6ad489ec40757",
+    "url": "/archanakarki/request_giosg_api.git/static/js/main.4390e9de.chunk.js"
+  },
+  {
+    "revision": "517f2553b8cebbe0faa7",
+    "url": "/archanakarki/request_giosg_api.git/static/js/runtime-main.a6d3beb3.js"
+  }
+]);
